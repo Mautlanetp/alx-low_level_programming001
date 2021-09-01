aaad0ccf373fc03e81bcf2fc0263db2eca6fd8c6
mautlanetp <blessingtshegofatsosibusiso@gmail.com>
2021-09-01 11:34:38 -0700

    0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - print helberton
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int length, i;
+
+	length = strlen(c);
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar(c[i];
+				}
+
+	_putcher('\n');
+
+	return (0);
+	}
