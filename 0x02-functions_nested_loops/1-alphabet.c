0a02bb8c3edd9b355480c2a11a11584cc3023ac5
mautlanetp <blessingtshegofatsosibusiso@gmail.com>
2021-09-01 19:06:58 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints lowercase alphabets
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+	
+	clrscr();
+
+	for (ch= 'a'; ch<= 'z'; ch++)
+		putchar(ch);
+		putchar ('\n');
+		getch()'
+}
