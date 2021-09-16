df2a3e53ff803725994300c4e064f9c6f6acd913
mautlanetp <blessingtshegofatsosibusiso@gmail.com>
2021-09-15 21:18:18 -0400

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,15 @@
  * @s: string
  * Return: no return.
  */
-void _puts_recursion(char *s);
+
+void _puts_recursion(char *s)
 {
-if (*s != '\0')
-{
+	if (*s != '\0')
+{	
 	_putchar(*s);
+
 	_puts_recursion(s + 1);
-}
-else
-_putchar('\n');
+	}
+	else
+	_putchar('\n');
 }
