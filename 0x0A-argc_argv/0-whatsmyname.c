a72eb032ebf5f0b5d0bd227916a1f29f968b8bb8
mautlanetp <blessingtshegofatsosibusiso@gmail.com>
2021-09-21 08:50:00 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - print the name of the program.
+ *
+ * @argc: counts the arguments supplied to th program.
+ *
+ * @argv: is an array of pointers to the string which are those arguments.
+ *
+ * Return: Always 0.
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 	printf("%s", argv[i]);
